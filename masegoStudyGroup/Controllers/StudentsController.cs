31c85fa6278fb1dad950e93578b1c5348b1dad38
u18221166 <u18221166@HOPGD-08>
2025-03-06 17:14:04 +0200

    Add project files.

---
+++ masegoStudyGroup/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace masegoStudyGroup.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult ListStudents()
+        {
+            List<Models.StudentModel> students = new List<Models.StudentModel>();
+
+            students.Add(new Models.StudentModel { StudentNumber = "u18221166", Name = "Masego", Surname = "Mahlangu", EmailAddress = "mc.mahlangu@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u21523516", Name = "Lesego", Surname = "Booi", EmailAddress = "lc.booi@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u22125364", Name = "Christopher", Surname = "Ditsehgo", EmailAddress = "ch.ditshego@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u27215364", Name = "Harry", Surname = "Styles", EmailAddress = "ht.styles@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u27351624", Name = "Taylor", Surname = "Swift", EmailAddress = "ts.swift@tuks.co.za" });
+
+            return View(students);
+        }
+    }
+}
---
+++ masegoStudyGroup/Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace masegoStudyGroup.Models
+{
+    public class StudentModel
+    {
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+    }
+}
